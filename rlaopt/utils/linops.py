78a25448bd8c23070e231370fc9eb3eb94d3b6c8
Pratik Rathore <pratikr@stanford.edu>
2025-03-18 16:47:50 -0700

    add more distributed lin op classes

--- rlaopt/utils/linops.py
+++ rlaopt/utils/linops.py
@@ -163,3 +163,135 @@ class DistributedLinOp(LinOp):
         matvecs = self._pool.map(self._worker, tasks)
         result = torch.cat(matvecs, dim=0)
         return result.to(w.device)
+
+
+class DistributedTwoSidedLinOp(TwoSidedLinOp):
+    def __init__(
+        self,
+        device_ids: List[Union[int, torch.device]],
+        A: List[TwoSidedLinOp],
+    ):
+        if not isinstance(device_ids, list):
+            raise ValueError("device_ids must be a list of device IDs.")
+        if not all(
+            isinstance(device_id, (int, torch.device)) for device_id in device_ids
+        ):
+            raise ValueError("device_ids must contain integers or torch devices.")
+        if not isinstance(A, list):
+            raise ValueError("A must be a list of linear operators.")
+        if not all(isinstance(A_i, TwoSidedLinOp) for A_i in A):
+            raise ValueError("All elements of A must be two-sided linear operators.")
+        if len(device_ids) != len(A):
+            raise ValueError("device_ids and A must have the same length.")
+
+        # Initialize members specific to distributed processing
+        self._A = A
+        self._device_ids = self._init_device_ids(device_ids)
+        self._pool = mp.Pool(processes=len(device_ids))
+
+        shape = (sum(A_i.shape[0] for A_i in A), A[0].shape[1])
+        super().__init__(
+            shape=shape,
+            matvec=self._matvec,
+            rmatvec=self._rmatvec,
+            matmat=None,
+            rmatmat=None,
+        )
+
+    def _init_device_ids(self, device_ids: List[Union[int, torch.device]]):
+        device_ids_corrected = []
+        for device_id in device_ids:
+            if isinstance(device_id, int):
+                device_ids_corrected.append(torch.device(f"cuda:{device_id}"))
+            elif isinstance(device_id, torch.device):
+                device_ids_corrected.append(device_id)
+        return device_ids_corrected
+
+    def _worker_matvec(self, task):
+        A_chunk, w_copy = task
+        return (A_chunk @ w_copy).cpu()
+
+    def _worker_rmatvec(self, task):
+        A_chunk, w_chunk = task
+        return (A_chunk.T @ w_chunk).cpu()
+
+    def _matvec(self, w: torch.Tensor):
+        w_copies = [w.to(device_id) for device_id in self._device_ids]
+        tasks = list(zip(self._A, w_copies))
+        matvecs = self._pool.map(self._worker_matvec, tasks)
+        result = torch.cat(matvecs, dim=0)
+        return result.to(w.device)
+
+    def _rmatvec(self, w: torch.Tensor):
+        # Split w based on A chunks
+        w_chunks = []
+        start_idx = 0
+        for i in range(len(self._device_ids)):
+            end_idx = start_idx + self._A[i].shape[1]
+            w_chunks.append(w[start_idx:end_idx].to(self._device_ids[i]))
+            start_idx = end_idx
+
+        A_T_chunks = [A_chunk.T for A_chunk in self._A]
+        tasks = list(zip(A_T_chunks, w_chunks))
+        rmatvecs = self._pool.map(self._worker_rmatvec, tasks)
+
+        # Since the result will be summed over all chunks and added to produce
+        # the final output vector
+        result = sum(rmatvecs)
+        return result.to(w.device)
+
+
+class DistributedSymmetricLinOp(DistributedTwoSidedLinOp):
+    def __init__(
+        self,
+        device_ids: List[Union[int, torch.device]],
+        A: List[TwoSidedLinOp],
+    ):
+        if not isinstance(device_ids, list):
+            raise ValueError("device_ids must be a list of device IDs.")
+        if not all(
+            isinstance(device_id, (int, torch.device)) for device_id in device_ids
+        ):
+            raise ValueError("device_ids must contain integers or torch devices.")
+        if not isinstance(A, list):
+            raise ValueError("A must be a list of linear operators.")
+        if not all(isinstance(A_i, TwoSidedLinOp) for A_i in A):
+            raise ValueError("All elements of A must be two-sided linear operators.")
+        if len(device_ids) != len(A):
+            raise ValueError("device_ids and A must have the same length.")
+        if not all(A_i.shape[0] == A_i.shape[1] for A_i in A):
+            raise ValueError("All elements of A must be square matrices.")
+
+        # Initialize members specific to distributed processing
+        self._A = A
+        self._device_ids = self._init_device_ids(device_ids)
+        self._pool = mp.Pool(processes=len(device_ids))
+
+        shape = (sum(A_i.shape[0] for A_i in A), A[0].shape[1])
+        super().__init__(
+            shape=shape,
+            matvec=self._matvec,
+            rmatvec=self._matvec,
+            matmat=None,
+            rmatmat=None,
+        )
+
+    def _init_device_ids(self, device_ids: List[Union[int, torch.device]]):
+        device_ids_corrected = []
+        for device_id in device_ids:
+            if isinstance(device_id, int):
+                device_ids_corrected.append(torch.device(f"cuda:{device_id}"))
+            elif isinstance(device_id, torch.device):
+                device_ids_corrected.append(device_id)
+        return device_ids_corrected
+
+    def _worker_matvec(self, task):
+        A_chunk, w_copy = task
+        return (A_chunk @ w_copy).cpu()
+
+    def _matvec(self, w: torch.Tensor):
+        w_copies = [w.to(device_id) for device_id in self._device_ids]
+        tasks = list(zip(self._A, w_copies))
+        matvecs = self._pool.map(self._worker_matvec, tasks)
+        result = torch.cat(matvecs, dim=0)
+        return result.to(w.device)
