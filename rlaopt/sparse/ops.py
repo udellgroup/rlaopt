c0128efd08e1d873a94c17131fac11ca6c4f86b8
Pratik Rathore <pratikr@stanford.edu>
2025-03-26 14:40:57 -0700

    checkpoint

--- rlaopt/sparse/ops.py
+++ rlaopt/sparse/ops.py
@@ -28,3 +28,34 @@ def _csc_matvec(sparse_tensor: Tensor, dense_vector: Tensor) -> Tensor:
         Tensor: The result of the multiplication.
     """
     return torch.ops.rlaopt.csc_matvec.default(sparse_tensor, dense_vector)
+
+
+def _csc_matmat(sparse_tensor: Tensor, dense_matrix: Tensor) -> Tensor:
+    """Multiply a sparse CSC tensor with a dense matrix.
+
+    Args:
+        sparse_tensor (Tensor): The sparse CSC tensor.
+        dense_matrix (Tensor): The dense matrix.
+
+    Returns:
+        Tensor: The result of the multiplication.
+    """
+    return torch.ops.rlaopt.csc_matmat.default(sparse_tensor, dense_matrix)
+
+
+def _csc_matmul(sparse_tensor: Tensor, dense_tensor: Tensor) -> Tensor:
+    """Multiply a sparse CSC tensor with a dense tensor.
+
+    Args:
+        sparse_tensor (Tensor): The sparse CSC tensor.
+        dense_tensor (Tensor): The dense tensor.
+
+    Returns:
+        Tensor: The result of the multiplication.
+    """
+    if dense_tensor.ndim == 1:
+        return _csc_matvec(sparse_tensor, dense_tensor)
+    elif dense_tensor.ndim == 2:
+        return _csc_matmat(sparse_tensor, dense_tensor)
+    else:
+        raise ValueError("The dense tensor must be 1D or 2D.")
