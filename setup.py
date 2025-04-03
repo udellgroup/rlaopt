a163448019b9b0dc53ff78ad346f464d4773cb0d
Pratik Rathore <pratikr@stanford.edu>
2025-04-02 19:24:33 -0700

    add linking to torch libraries in extension build

--- setup.py
+++ setup.py
@@ -1,3 +1,82 @@
+# import os
+# import torch
+# import glob
+# from setuptools import find_packages, setup
+# from torch.utils.cpp_extension import (
+#     CppExtension,
+#     CUDAExtension,
+#     BuildExtension,
+#     CUDA_HOME,
+# )
+
+# LIBRARY_NAME = "rlaopt"
+
+# if torch.__version__ >= "2.6.0":
+#     py_limited_api = True
+# else:
+#     py_limited_api = False
+
+
+# def get_extensions():
+#     debug_mode = os.getenv("DEBUG", "0") == "1"
+#     use_cuda = os.getenv("USE_CUDA", "1") == "1"
+#     use_openmp = os.getenv("USE_OPENMP", "1") == "1"
+#     if debug_mode:
+#         print("Compiling in debug mode")
+
+#     use_cuda = use_cuda and torch.cuda.is_available() and CUDA_HOME is not None
+#     extension = CUDAExtension if use_cuda else CppExtension
+
+#     extra_link_args = []
+#     extra_compile_args = {
+#         "cxx": [
+#             "-O3" if not debug_mode else "-O0",
+#             "-fdiagnostics-color=always",
+#             "-DPy_LIMITED_API=0x03090000",  # min CPython version 3.9
+#         ],
+#         "nvcc": [
+#             "-O3" if not debug_mode else "-O0",
+#         ],
+#     }
+
+#     if use_openmp:
+#         extra_compile_args["cxx"].append("-fopenmp")
+#         extra_link_args.append("-fopenmp")
+
+#     if debug_mode:
+#         extra_compile_args["cxx"].append("-g")
+#         extra_compile_args["nvcc"].append("-g")
+#         extra_link_args.extend(["-O0", "-g"])
+
+#     this_dir = os.path.dirname(os.path.curdir)
+#     extensions_dir = os.path.join(this_dir, LIBRARY_NAME, "csrc")
+#     sources = list(glob.glob(os.path.join(extensions_dir, "*.cpp")))
+
+#     extensions_cuda_dir = os.path.join(extensions_dir, "cuda")
+#     cuda_sources = list(glob.glob(os.path.join(extensions_cuda_dir, "*.cu")))
+
+#     if use_cuda:
+#         sources += cuda_sources
+
+#     ext_modules = [
+#         extension(
+#             f"{LIBRARY_NAME}._C",
+#             sources,
+#             extra_compile_args=extra_compile_args,
+#             extra_link_args=extra_link_args,
+#             py_limited_api=py_limited_api,
+#         )
+#     ]
+
+#     return ext_modules
+
+
+# setup(
+#     packages=find_packages(),
+#     ext_modules=get_extensions(),
+#     cmdclass={"build_ext": BuildExtension},
+#     options={"bdist_wheel": {"py_limited_api": "cp39"}} if py_limited_api else {},
+# )
 import os
 import torch
 import glob
@@ -27,7 +106,14 @@ def get_extensions():
     use_cuda = use_cuda and torch.cuda.is_available() and CUDA_HOME is not None
     extension = CUDAExtension if use_cuda else CppExtension
 
-    extra_link_args = []
+    # Get PyTorch library path for RPATH
+    torch_lib_path = os.path.join(os.path.dirname(torch.__file__), "lib")
+
+    extra_link_args = [
+        # Add RPATH to ensure PyTorch libraries can be found at runtime
+        f"-Wl,-rpath,{torch_lib_path}"
+    ]
+
     extra_compile_args = {
         "cxx": [
             "-O3" if not debug_mode else "-O0",
@@ -64,6 +150,8 @@ def get_extensions():
             sources,
             extra_compile_args=extra_compile_args,
             extra_link_args=extra_link_args,
+            library_dirs=[torch_lib_path],  # Add PyTorch library directory
+            runtime_library_dirs=[torch_lib_path],  # Add runtime path (RPATH)
             py_limited_api=py_limited_api,
         )
     ]
