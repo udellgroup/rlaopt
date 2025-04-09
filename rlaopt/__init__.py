b20a74334e406ff892a912c7452102c8d8dfc337
Pratik Rathore <76628577+pratikrathore8@users.noreply.github.com>
2025-04-09 14:32:14 -0700

    add torch import in __init__.py (#44)

--- rlaopt/__init__.py
+++ rlaopt/__init__.py
@@ -1,2 +1,5 @@
-# import C++ and CUDA extensions
+# Import torch to ensure its libraries are linked properly for the extensions
+import torch  # noqa: F401
+
+# Now, import C++ and CUDA extensions
 from . import _C  # noqa: F401
