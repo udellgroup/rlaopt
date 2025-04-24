d5eec6dcb797ed54f922e8db692e548852e2b399
Pratik Rathore <pratikr@stanford.edu>
2025-04-23 21:04:48 -0700

    move creation of _C module to module.cpp

--- rlaopt/csrc/cpp/row_slice.cpp
+++ rlaopt/csrc/cpp/row_slice.cpp
@@ -1,26 +1,7 @@
 #include <ATen/Operators.h>
-#include <Python.h>
 #include <torch/library.h>
 
 #include "../cpp_include/input_checks.h"
-
-extern "C" {
-/* Creates a dummy empty _C module that can be imported from Python.
-   The import from Python will load the .so consisting of this file
-   in this extension, so that the TORCH_LIBRARY static initializers
-   below are run. */
-PyObject* PyInit__C(void) {
-    static struct PyModuleDef module_def = {
-        PyModuleDef_HEAD_INIT,
-        "_C", /* name of module */
-        NULL, /* module documentation, may be NULL */
-        -1,   /* size of per-interpreter state of the module,
-                 or -1 if the module keeps state in global variables. */
-        NULL, /* methods */
-    };
-    return PyModule_Create(&module_def);
-}
-}
 
 namespace rlaopt {
 
