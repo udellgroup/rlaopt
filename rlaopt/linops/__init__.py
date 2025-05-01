f00bf4c17d10c6ab471084f183dab31939cec120
Pratik Rathore <pratikr@stanford.edu>
2025-04-30 22:46:44 -0700

    get rid of scalelinop

--- rlaopt/linops/__init__.py
+++ rlaopt/linops/__init__.py
@@ -1,12 +1,11 @@
 """Linops module __init__.py file."""
 from .distributed import *
 from .mixins import *
-from .scale import *
 from .simple import *
 from .types import *
 
 # Collect __all__ from imported modules
 __all__ = []
-for module in [distributed, mixins, scale, simple, types]:
+for module in [distributed, mixins, simple, types]:
     if hasattr(module, "__all__"):
         __all__.extend(module.__all__)
