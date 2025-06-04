fb3163d1b3de30033b15b1ce5bd31bd798a70e1f
Pratik Rathore <pratikr@stanford.edu>
2025-06-04 14:53:35 -0700

    remove old source code

--- rlaopt/sketches/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Sketches module __init__.py file."""
-from .factory import *
-
-# Collect __all__ from imported modules
-__all__ = []
-for module in [factory]:
-    if hasattr(module, "__all__"):
-        __all__.extend(module.__all__)
