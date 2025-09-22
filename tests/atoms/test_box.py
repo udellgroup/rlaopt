1313bf8fb0f9dbad0a7757905acd7f04292feee1
Zachary Frangella <zfrangella@alumni.stanford.edu>
2025-09-22 10:33:31 -0700

    Add tests for solvers and atoms

---
+++ tests/atoms/test_box.py
@@ -0,0 +1,95 @@
+import pytest
+import torch
+
+from rlaopt.atoms.sum_squares import SumSquares
+from rlaopt.atoms.non_negative import Box
+from rlaopt.expression.expression import Variable
+from rlaopt.solvers.configs import ProxGradConfig
+from rlaopt.solvers.proximal_gradient.prox_grad import ProximalGradient
+
+
+torch.manual_seed(0)
+
+TOLERANCES = {torch.float32: 1e-6, torch.float64: 1e-10}
+
+@pytest.fixture(params=[torch.float32, torch.float64], ids=["float32", "float64"])
+def precision(request):
+    return request.param
+
+@pytest.fixture
+def test_dim():
+    return 128
+
+@pytest.fixture
+def test_var(test_dim, precision):
+    x = Variable(torch.zeros(test_dim, dtype=precision))
+    return x
+
+@pytest.fixture
+def l():
+    return torch.tensor(-2.0)
+
+@pytest.fixture
+def u():
+    return torch.tensor(1.0)
+
+@pytest.fixture
+def tol(precision):
+    return TOLERANCES[precision]
+
+class TestBoxBasics:
+    def test_init(self, test_var, l, u):
+        r = Box(test_var, l=l, u=u)
+        assert r.x is not None
+    
+    def test_forward(self, test_var, l, u, test_dim, precision):
+        r = Box(test_var, l=l, u=u)
+        assert r.forward() == 0.0
+
+        params = {"x": -3 * torch.ones(test_dim, dtype=precision)}
+        assert r.evaluate(params) == torch.inf
+
+    def test_prox(self, test_var, l, u, test_dim, precision):
+        r = Box(test_var, l=l, u=u)
+        
+        v = torch.ones(test_dim, dtype=precision)
+        scaling_factor = 1.0
+        
+        prox = r.prox(-v, scaling_factor)
+        assert (prox == -v).all() == True
+
+        prox = r.prox(-4 * v, scaling_factor)
+        assert (prox == -2.0 * v).all() == True
+    
+
+class TestBoxSolve:
+    
+    def test_prox_grad(self, l, u, precision, tol):
+        torch.set_default_dtype(precision)
+        
+        n, p = 1024, 256
+        A = torch.randn((n, p)) / n ** (0.5)
+        b = torch.randn(n) / n ** (0.5)
+        x = Variable(torch.zeros(p))
+        
+        obj = SumSquares(A @ x - b) + Box(x, l=l, u=u)
+        
+        eta = 0.5 / torch.linalg.norm(A, ord=2) ** 2
+       
+        config = ProxGradConfig(eta=eta, use_acceleration=True, use_linesearch=False)
+        opt = ProximalGradient(config, obj)
+        params = obj.params
+        state = opt.init_state(params)
+
+        for _ in range(500):
+            params, state = opt.step(params, state)
+        err = state.err
+
+        names = list(params.keys())
+        params = params[names[0]]
+        
+        # Params are in the box 
+        assert (params >= l).all() & (params <= u).all() == True
+
+        # Problem is solved
+        assert (err.item() <= tol) == True
