f006205d18f061e09041e5167ca84062ea77ab8d
Pratik Rathore <76628577+pratikrathore8@users.noreply.github.com>
2025-10-07 18:46:29 -0700

    update dependencies, update pre-commit, fix failing tests, continue improving atoms (#61)
    * simplify author list in pyproject.toml
    
    * add cvxpy to pyproject.toml
    
    * >- -> >= for cvxpy
    
    * update descriptions/keywords in pyproject.toml, add rlaopt.egg-info to .gitignore
    
    * clean up README.md
    
    * update requirements-dev.txt
    
    * remove references to C/C++ in pre commit config + sonar project properties
    
    * clear out old dependencies
    
    * change pre commit and pyproject toml to use ruff
    
    * fix tests, update nuclear norm atom, add elastic_net atom (#64)
    
    Co-authored-by: Zachary Frangella <zfrangella@stanford.alumni.edu>
    
    * sonarqube v5 -> sonarqube v6
    
    ---------
    
    Co-authored-by: Zachary Frangella <zfrangella@alumni.stanford.edu>
    Co-authored-by: Zachary Frangella <zfrangella@stanford.alumni.edu>

--- rlaopt/atoms/nuc_norm.py
+++ rlaopt/atoms/nuc_norm.py
@@ -7,17 +7,21 @@ from rlaopt.atoms.atom_expression import AtomExpression
 from rlaopt.expression.expression import Variable
 from rlaopt.atoms._utils import get_variable_value
 
-class NucNorm(AtomExpression):
 
-    def __init__(self, x: Variable, scaling: float | torch.Tensor | torch.nn.Parameter = 1.0):
+class NucNorm(AtomExpression):
+    def __init__(
+        self, x: Variable, scaling: float | torch.Tensor | torch.nn.Parameter = 1.0
+    ):
         super().__init__()
 
         if not isinstance(x, Variable):
             raise TypeError(f"Expected Variable, got {type(x)}")
-        
+
         if x.value.data.dim() != 2:
-            raise ValueError(f"Variable value must be 2D Tensor, but got {x.value.data.dim()}D Tensor.")
-        
+            raise ValueError(
+                f"Variable value must be 2D Tensor, but got {x.value.data.dim()}D Tensor."
+            )
+
         # Register the variable as a parameter
         self.register_parameter("x", x.value)
 
@@ -28,28 +32,28 @@ class NucNorm(AtomExpression):
             self.register_buffer("scaling", scaling.data)
         else:
             self.register_buffer("scaling", torch.tensor(float(scaling)))
-        
+
     def is_smooth(self):
-            return False
-    
+        return False
+
     def evaluate_at(self, **variable_locations):
-         value = get_variable_value(self.x, **variable_locations) 
-         S = torch.linalg.svdvals(value)
-         return self.scaling * torch.sum(S)
-    
+        value = get_variable_value(self.x, **variable_locations)
+        S = torch.linalg.svdvals(value)
+        return self.scaling * torch.sum(S)
+
     def is_proxable(self):
-         return True
-    
+        return True
+
     def prox(self, location, prox_scaling):
-         U, S, Vt = torch.linalg.svd(location, full_matrices=False)
-         S = torch.nn.functional.relu(S - prox_scaling * self.scaling)
-         return U @ (S[:, None] * Vt)
-    
+        U, S, Vt = torch.linalg.svd(location, full_matrices=False)
+        S = torch.nn.functional.relu(S - prox_scaling * self.scaling)
+        return (U * S) @ Vt
+
     def is_subsamplable(self):
-         return False
-    
+        return False
+
     def subsample(self, indices):
-         raise NotImplementedError("Nuclear norm cannot be subsampled")
-    
+        raise NotImplementedError("Nuclear norm cannot be subsampled")
+
     def to_cvxpy(self):
-         raise NotImplementedError("NucNorm does not support conversion to cvxpy")
+        raise NotImplementedError("NucNorm does not support conversion to cvxpy")
