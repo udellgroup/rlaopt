5dd07656a12dcdcc2172157905557ea363a2a933
Pratik Rathore <pratikr@stanford.edu>
2025-10-15 17:52:33 -0700

    add prox grad stuff to rlaopt.solvers.__init__

---
+++ rlaopt/solvers/__init__.py
@@ -0,0 +1,4 @@
+"""Solver implementations and configurations."""
+
+from rlaopt.solvers.prox_grad import ProxGradConfig as ProxGradConfig
+from rlaopt.solvers.prox_grad import ProximalGradient as ProximalGradient
