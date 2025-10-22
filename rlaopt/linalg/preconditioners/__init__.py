b56c852ea2e28badbb6fc77cac100526edac6d3e
Pratik Rathore <pratikr@stanford.edu>
2025-10-21 17:35:44 -0700

    checkpoint

--- rlaopt/linalg/preconditioners/__init__.py
+++ rlaopt/linalg/preconditioners/__init__.py
@@ -1,5 +1,11 @@
 """Preconditioner implementations."""
 
+from rlaopt.linalg.preconditioners.identity import (
+    Identity as Identity,
+)
+from rlaopt.linalg.preconditioners.identity import (
+    IdentityConfig as IdentityConfig,
+)
 from rlaopt.linalg.preconditioners.preconditioner import (
     Preconditioner as Preconditioner,
 )
