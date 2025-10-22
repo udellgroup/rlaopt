49db292eee652e9915d586ca913a998e31c430bc
Pratik Rathore <pratikr@stanford.edu>
2025-10-22 14:09:34 -0700

    checkpoint

--- rlaopt/linalg/__init__.py
+++ rlaopt/linalg/__init__.py
@@ -1,4 +1,5 @@
 """Module for linear algebra."""
 
 from rlaopt.linalg.lin_sys import LinSys as LinSys
+from rlaopt.linalg.preconditioners import PreconditionerConfig as PreconditionerConfig
 from rlaopt.linalg.preconditioners import get_preconditioner as get_preconditioner
--- rlaopt/linalg/preconditioners/__init__.py
+++ rlaopt/linalg/preconditioners/__init__.py
@@ -3,3 +3,6 @@
 from rlaopt.linalg.preconditioners.factory import (
     get_preconditioner as get_preconditioner,
 )
+from rlaopt.linalg.preconditioners.preconditioner import (
+    PreconditionerConfig as PreconditionerConfig,
+)
