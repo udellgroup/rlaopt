6abeac540839fe18078d13232228182e8876dbf9
Zachary Frangella <zfrangella@stanford.alumni.edu>
2025-10-27 10:27:22 -0700

    Change style of imports in __init__ files to be consistent with other modules.

--- rlaopt/atoms/__init__.py
+++ rlaopt/atoms/__init__.py
@@ -1,31 +1,13 @@
 """__init__.py file for atoms module."""
 
-from rlaopt.atoms.affine import Affine
-from rlaopt.atoms.atom_expression import AtomExpression
-from rlaopt.atoms.box import Box
-from rlaopt.atoms.elastic_net import ElasticNet
-from rlaopt.atoms.halfspace import Halfspace
-from rlaopt.atoms.l1norm import L1Norm
-from rlaopt.atoms.linear_equality import LinearEquality
-from rlaopt.atoms.non_negative import NonNegative
-from rlaopt.atoms.nuc_norm import NucNorm
-from rlaopt.atoms.polyhedron import Polyhedron
-from rlaopt.atoms.sum_squares import SumSquares
-
-__all__ = [
-    # Atoms Base
-    "AtomExpression",
-    # Constraints
-    "Box",
-    "Halfspace",
-    "LinearEquality",
-    "NonNegative",
-    "Polyhedron",
-    # Regularizers
-    "ElasticNet",
-    "L1Norm",
-    "NucNorm",
-    # Math Expressions
-    "Affine",
-    "SumSquares",
-]
+from rlaopt.atoms.affine import Affine as Affine
+from rlaopt.atoms.atom_expression import AtomExpression as AtomExpression
+from rlaopt.atoms.box import Box as Box
+from rlaopt.atoms.elastic_net import ElasticNet as ElasticNet
+from rlaopt.atoms.halfspace import Halfspace as Halfspace
+from rlaopt.atoms.l1norm import L1Norm as L1Norm
+from rlaopt.atoms.linear_equality import LinearEquality as LinearEquality
+from rlaopt.atoms.non_negative import NonNegative as NonNegative
+from rlaopt.atoms.nuc_norm import NucNorm as NucNorm
+from rlaopt.atoms.polyhedron import Polyhedron as Polyhedron
+from rlaopt.atoms.sum_squares import SumSquares as SumSquares
