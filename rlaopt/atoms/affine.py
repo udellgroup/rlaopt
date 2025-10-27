51b8d9e4549ce198ec6f1248445496e7b7db62d0
Zachary Frangella <zfrangella@stanford.alumni.edu>
2025-10-27 10:28:47 -0700

    Change input to input_ so variable name does not shadow python built-in.

--- rlaopt/atoms/affine.py
+++ rlaopt/atoms/affine.py
@@ -61,11 +61,11 @@ class Affine(AtomExpression):
         Returns:
             torch.Tensor: Result of A @ x + b.
         """
-        input = self.get_input()
-        if isinstance(input, torch.nn.Parameter):
-            return self.A @ input + self.b
-        elif isinstance(input, Expression):
-            return self.A @ input.forward() + self.b
+        input_ = self.get_input()
+        if isinstance(input_, torch.nn.Parameter):
+            return self.A @ input_ + self.b
+        elif isinstance(input_, Expression):
+            return self.A @ input_.forward() + self.b
 
     def is_proxable(self) -> bool:
         """Check if the affine transformation has a computable proximal operator.
